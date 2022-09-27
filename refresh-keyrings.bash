92cff22c6ef319b52c5a3e06a656f14e92c17fed
nonie689 <nonie689 at eclipso [dot] ch>
2022-09-27 10:30:30 +0200

    Tweak refresh-keyrings! Checking for forced or classic updategit add --all

--- refresh-keyrings.bash
+++ refresh-keyrings.bash
@@ -1,13 +1,20 @@
 
-echo
-echo "Refreshing keyrings -- forced mode!!"
-echo
-sudo rm -rf /etc/pacman.d/gnupg
-sudo pacman-key --init
-sudo pacman-key --populat
-sudo pacman -Sy archlinux-keyring chaotic-keyring --noconfirm
+function forced-refresh {
+	echo "[))> Using fallback -- [forced mode]--"
+	echo
+
+	sudo rm -rf /etc/pacman.d/gnupg
+	sudo pacman-key --init
+	sudo pacman-key --populat
+	sudo pacman -Sy archlinux-keyring chaotic-keyring --noconfirm
+}
 
 echo
-echo "All done!!"
+echo "Refresh keyrings!!"
+echo
+
+sudo pacman -Sy archlinux-keyring chaotic-keyring --noconfirm 2>/dev/null || forced-refresh
+
+echo "[))> All done!!"
 echo
 
